c2931cc3b06530e7995fa4efeb56f96235fe5656
Jinwoo Kim <kidjinwoo@me.com>
2023-09-30 19:56:50 +0900

    support for rootless

--- Makefile
+++ Makefile
@@ -1,5 +1,6 @@
 TARGET := iphone:clang:latest
 INSTALL_TARGET_PROCESSES = SpringBoard
+THEOS_PACKAGE_SCHEME = rootless
 export ARCHS = arm64 arm64e
 
 include $(THEOS)/makefiles/common.mk
--- init.m
+++ init.m
@@ -14,8 +14,13 @@ void custom_UIStaticBatteryView_setShowsPercentage(_UIStaticBatteryView *self, S
     original_UIStaticBatteryView_setShowsPercentage(self, cmd, YES);
 }
 
+BOOL custom_UIStaticBatteryView_showsPercentage(_UIStaticBatteryView *self, SEL cmd) {
+    return YES;
+}
+
 __attribute__((constructor)) static void init() {
     @autoreleasepool {
         MSHookMessageEx(NSClassFromString(@"_UIStaticBatteryView"), @selector(setShowsPercentage:), (IMP)&custom_UIStaticBatteryView_setShowsPercentage, (IMP *)&original_UIStaticBatteryView_setShowsPercentage);
+        MSHookMessageEx(NSClassFromString(@"_UIStaticBatteryView"), @selector(showsPercentage), (IMP)&custom_UIStaticBatteryView_showsPercentage, NULL);
     }
 }
